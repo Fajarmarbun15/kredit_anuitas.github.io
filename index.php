98ea78b99563fc804eab3fc28a439a1ae3faf782
Fajarmarbun15 <fajarmarbun1@gmail.com>
2022-10-30 16:37:48 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simulasi Kredit</title>
+</head>
+<body>
+ 
+    
+<form action="anuitas.php" method="post">
+    <label>Besar Pinjaman</label> : <input type="text" name="besar_pinjaman" placeholder="masukan angka tanpa titik">
+    <label>Bunga (%)</label> : <input type="text" name="bunga"  value="17">
+    <label>Lama Pinjaman (bulan)</label> : <input type="text" name="jangka" placeholder="masukan jangka waktu peminjaman">
+    <button type="submit">Simulasikan</button>
+</form>
+ 
+ 
+</body>
+</html><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span>
